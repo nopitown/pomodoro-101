138b31de9cb6f97291ada6ff2836a5f0b421a2ca
Elizabeth Manrique <ecmanrique+nopito@gmail.com>
2020-03-11 22:00:57 -0500

    Use state machine to control state of the timer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Machine } from "xstate";
+import { useMachine } from "@xstate/react";
+
 import Timer from "../Timer";
 import Controls from "../Controls";
 
@@ -6,12 +9,29 @@ const ONE_SECOND = 1000; // milliseconds
 const ONE_MINUTE = 60 * ONE_SECOND;
 const DEFAULT_LENGTH = ONE_MINUTE * 25;
 
+const stateTimerMachine = Machine({
+  id: "timer",
+  initial: "idle",
+  states: {
+    idle: {
+      on: {
+        START: "started"
+      }
+    },
+    started: {
+      on: { PAUSE: "paused", RESET: "idle" }
+    },
+    paused: {
+      on: { START: "started", RESET: "idle" }
+    }
+  }
+});
 function App() {
   const [currentTime, setCurrentTime] = useState(DEFAULT_LENGTH);
-  const [running, setRunning] = useState(false);
+  const [state, send] = useMachine(stateTimerMachine);
 
   useEffect(() => {
-    if (running) {
+    if (state.value === "started") {
       const updateTimer = setTimeout(() => {
         if (currentTime !== 0) {
           setCurrentTime(currentTime - ONE_SECOND);
@@ -19,13 +39,22 @@ function App() {
       }, ONE_SECOND);
       return () => clearTimeout(updateTimer);
     }
-  }, [currentTime, running]);
+
+    if (state.value === "idle") {
+      setCurrentTime(DEFAULT_LENGTH);
+    }
+  }, [currentTime, state]);
 
   return (
     <main className="App">
       <h1>Pomodoro Timer</h1>
       <Timer currentTime={currentTime} />
-      <Controls onPlay={() => setRunning(true)} />
+      <Controls
+        onPlay={() => send("START")}
+        onPause={() => send("PAUSE")}
+        onReset={() => send("RESET")}
+        status={state.value}
+      />
     </main>
   );
 }
--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-const Controls = ({ onPlay, onReset }) => {
+const Controls = ({ onPlay, onPause, onReset, status }) => {
+  const handleClickPlayOrPause = () => {
+    if (status === "idle" || status === "paused") {
+      onPlay();
+    } else {
+      onPause();
+    }
+  };
+
   return (
     <div>
-      <button onClick={onPlay}>Play</button>
+      <button onClick={handleClickPlayOrPause}>
+        {status === "idle" || status === "paused" ? "Play" : "Pause"}
+      </button>
       <button onClick={onReset}>Reset</button>
     </div>
   );
